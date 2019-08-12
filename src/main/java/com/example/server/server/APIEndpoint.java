cefab3b11fcf5067029d1607db9210bc364c2934
Mike Hake <mike.hake@billgo.com>
2019-08-12 11:09:04 -0600

    small simplification

--- src/main/java/com/example/server/server/APIEndpoint.java
+++ src/main/java/com/example/server/server/APIEndpoint.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,19 +15,25 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 @RestController
-@RequestMapping("/rest/generic")
+@RequestMapping("/api")
 public class APIEndpoint {
   private static final Logger log = LoggerFactory.getLogger(APIEndpoint.class);
 
-  @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-  ResponseEntity<?> genericPost(HttpServletRequest request) throws IOException {
-    final String json = converToString(request.getInputStream());
+  @RequestMapping(method = RequestMethod.GET)
+  public ResponseEntity<String> get(@RequestParam String name) {
+    log.info("/api GET invoked. Name = {}", name);
+    return new ResponseEntity<>("Hello " + name, HttpStatus.OK);
+  }
 
-    log.info("/rest/generic POST invoked. JSON = {}", json);
+  @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> post(HttpServletRequest request) throws IOException {
+    final String json = convertToString(request.getInputStream());
+
+    log.info("/api POST invoked. JSON = {}", json);
     return new ResponseEntity<Object>(null, HttpStatus.OK);
   }
 
-  private String converToString(InputStream inputStream) throws IOException{
+  private String convertToString(InputStream inputStream) throws IOException{
     StringBuilder textBuilder = new StringBuilder();
     try (Reader reader = new BufferedReader(new InputStreamReader
             (inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
